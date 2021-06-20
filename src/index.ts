5b4e4b9a66fb8a55a7ec8d3b2665e28043ddbd37
Lorant Domokos <domokos.lorant@yahoo.com>
2021-06-20 01:42:40 -0700

    Updates

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ export * from "./events/ticket-updated-event";
 export * from "./events/types/order-status";
 export * from "./events/order-created-event";
 export * from "./events/order-cancelled-event";
+export * from "./events/expiration-complete-event";
 
 // Utils
 export * from "./utils/propertyOf";
